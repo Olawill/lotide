423af7c6b54120661d1e8ffffacf1a69e33de25a
Olawale <henry.williams658@gmail.com>
2023-06-22 04:52:23 +0000

    Added mapping file to repo

---
+++ map.js
@@ -0,0 +1,43 @@
+// ASSERTION TEST CODES:::
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+      return true
+    }
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] is a perfect match to [${arr2}]`);
+    return;
+  }
+  console.log(`🔴🔴🔴 Assertion Failed: [${arr1}] is not a perfect match to [${arr2}]`);
+};
+
+// Words to test with
+const words = ["ground", "control", "to", "major", "tom"];
+
+// arr, functionToMap
+const map = (arr, functionToMap) => {
+
+  const results = [];
+  for (let item of arr) {
+    results.push(functionToMap(item));
+  }
+  return results;
+};
+
+const result1 = map(words, word => word[0]);
+const result2 = map(words, word => word.length);
+
+console.log(result1);
+
+
+// TEST CODES
+assertArraysEqual(map(words, word => word[0]), result1);
+assertArraysEqual(map(words, word => word.length), result2);
