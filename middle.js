df6f9c8d5a396a8c6ecff9e395d55ddfa34d76e5
Olawale <henry.williams658@gmail.com>
2023-06-22 03:06:21 +0000

    Added file to check if two objects are the same, also updated the flatten and middle files

--- middle.js
+++ middle.js
@@ -1,12 +1,11 @@
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
-    let perfectMatch = true;
     for (let i = 0; i < arr1.length; i++) {
-      if (perfectMatch) {
-        perfectMatch = arr1[i] === arr2[i];
+      if (arr1[i] !== arr2[i]) {
+        return false;
       }
     }
-    return perfectMatch;
+    return true;
   }
   return false;
 };
