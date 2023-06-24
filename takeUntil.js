11511e402c66dd0695298b47bf71c50a74810349
Olawale <henry.williams658@gmail.com>
2023-06-24 15:05:59 +0000

    Added file to filter array until condition specified is met

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+// TAKE UP UNTIL FUNCTION
+const takeUntil = function(array, callback) {
+  // ...
+  let newArr = [];
+  for (const e of array) {
+    if (callback(e)) {
+      return newArr;
+    } 
+    newArr.push(e);
+  }
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+// ASSERTION CODE & TEST CASES
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    let perfectMatch = true;
+    for (let i = 0; i < arr1.length; i++) {
+      if (perfectMatch) {
+        perfectMatch = arr1[i] === arr2[i];
+      }
+    }
+    return perfectMatch;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const inspect = require('util').inspect;
+  if (eqArrays(arr1, arr2)) {
+
+    console.log(`✅✅✅Assertion Passed: ${inspect(arr1)} is a perfect match to ${inspect(arr2)}`);
+    return;
+  }
+  console.log(`🔴🔴🔴Assertion Failed: ${inspect(arr1)} is not a perfect match to ${inspect(arr2)}`);
+};
+
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
