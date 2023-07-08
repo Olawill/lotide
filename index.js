53be211cfb119814a216a1911c4e91a9894c6004
Olawale <henry.williams658@gmail.com>
2023-07-08 04:43:32 +0000

    Refactored index.js file to use ES6 shorthand for objects

--- index.js
+++ index.js
@@ -2,34 +2,9 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const assertEqual   = require('./assertEqual');
-// const assertArraysEqual   = require('./assertArraysEqual');
-// const assertObjectEqual = require('./assertObjectEqual');
-// const countLetters   = require('./countLetters');
-// const countOnly   = require('./countOnly');
 const eqArrays = require('./eqArrays');
-// const eqObjects   = require('./eqObjects');
-// const findKey   = require('./findKey');
-// const findKeyByValue = require('./findKeyByValue');
-// const flatten = require('./flatten');
-// const letterPositions   = require('./letterPositions');
-// const takeUntil   = require('./takeUntil');
-// const without = require('./without');
+
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  // assertArraysEqual: assertArraysEqual,
-  // assertObjectEqual: assertObjectEqual,
-  // countLetters: countLetters,
-  // countOnly: countOnly,
-  eqArrays: eqArrays,
-  // eqObjects: eqObjects,
-  // findKey: findKey,
-  // findKeyByValue: findKeyByValue,
-  // flatten: flatten,
-  // letterPositions: letterPositions,
-  // takeUntil: takeUntil,
-  // without: without
+  head, tail, middle, assertEqual, eqArrays
 };
